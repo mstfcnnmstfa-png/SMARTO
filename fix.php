71160280116001f05d80541d1233f3cb63b3dd63
mstfcnnmstfa-png <mstfcnnmstfa@gmail.com>
2026-04-03 11:28:57 +0300

    Create fix.php

---
+++ fix.php
@@ -0,0 +1,12 @@
+<?php
+$files = ['Namero.php', 'polling.php', 'service.php', 'admin_login.php'];
+foreach ($files as $file) {
+    $content = file_get_contents($file);
+    $fixed = str_replace(
+        "8076347498:AAEq520a0raqgxY\xc3\x980kQW7_fiYM23khnxSKNU",
+        "YOUR_NEW_TOKEN_HERE",
+        $content
+    );
+    file_put_contents($file, $fixed);
+    echo "Fixed: $file\n";
+}
